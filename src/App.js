566ba92a103d60cb511cc998cc610d97313ccc7c
claudio.poli <claudio.poli@infocube.it>
2022-05-19 02:25:45 +0200

    user reducer in redux

--- src/App.js
+++ src/App.js
@@ -4,8 +4,25 @@ import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
 import Shop from "./routes/shop/shop.component";
 import Checkout from "./routes/checkout/checkout.component";
+import { useEffect } from "react";
+import { onAuthStateChangedListener, createUserDocumentFromAuth } from "./utils/firebase/firebase.utils";
+import { useDispatch } from "react-redux";
+import { USER_ACTION_TYPES } from "./store/user/user.types";
 
 const App = () => {
+  // Passa l'azione al redux store, il quale la passa a tutti i reducers 
+  const dispatch = useDispatch();
+  // Configura l'utente corrente ad ogni signin/signup e lo resetta al signout tramite unsubscribe
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      if (user) {
+        createUserDocumentFromAuth(user);
+      }
+      dispatch({type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user});
+    });
+    return unsubscribe;
+  }, [dispatch])
+
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
---
+++ src/store/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { logger } from 'redux-logger';
+import { userReducer } from './user/user.reducer';
+
+export const store = configureStore({
+  reducer: {
+    user: userReducer
+  },
+  middleware: [logger]
+})
