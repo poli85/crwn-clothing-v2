4504a8229448a967d358271f6d682ea6c63d3db5
claudio.poli <claudio.poli@infocube.it>
2022-05-28 14:20:30 +0200

    redux persist and devtools

--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,34 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { logger } from 'redux-logger';
+import persistReducer from 'redux-persist/es/persistReducer';
+import persistStore from 'redux-persist/es/persistStore';
+import storage from 'redux-persist/lib/storage';
 import { cartReducer } from './cart/cart.reducer';
 import { categoriesReducer } from './categories/categories.reducer';
 import { userReducer } from './user/user.reducer';
+import { combineReducers } from 'redux';
+
+const reducers = combineReducers({
+  user: userReducer,
+  categories: categoriesReducer,
+  cart: cartReducer
+})
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['user']
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const middleware = process.env.NODE_ENV === 'development' ? [logger] : [];
+
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-    categories: categoriesReducer,
-    cart: cartReducer
-  },
-  middleware: [logger]
-})
+  reducer: persistedReducer,
+  middleware,
+  devTools: process.env.NODE_ENV === 'development'
+});
+
+export const persistor = persistStore(store);
